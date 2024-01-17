d0f28303ce2e42b8950f7ce5272094aa05354e2c
lliolla <lliolla@hotmail.com>
2024-01-17 19:14:17 +0100

    FEAT :fonction login ok

--- budget-tracker-serveur/controllers/user.js
+++ budget-tracker-serveur/controllers/user.js
@@ -32,27 +32,35 @@ bcrypt.hash(req.body.password,10)
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })// on cherche l'utilisateur qui correspond aux mail ds BD
-    // on verif qu'on a un user sinon erreur
-    .then(user => {
-        if (!user) {
-            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        }//on comparer le mot de passe de la requette avec le mp hashe
-    bcrypt.compare(req.body.password, user.password)
-        .then(valid => {// return true ou false
-            if (!valid) {
-                return res.status(401).json({ error: 'Mot de passe incorrect !' });
+    User.findOne({ name: req.body.name })
+        .then(user => {
+            console.log('user:', user);
+
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
-            res.status(200).json({
-                userId: user._id,
-                token: jwt.sign(
-                    { userId: user._id },
-                    'RANDOM_TOKEN_SECRET',
-                    { expiresIn: '24h' }
-                )
-            });
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
+
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => {
+                    console.error('Erreur lors de la comparaison du mot de passe:', error);
+                    res.status(500).json({ error });
+                });
         })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+        .catch(error => {
+            console.error('Erreur lors de la recherche de l\'utilisateur:', error);
+            res.status(500).json({ error });
+        });
 };
