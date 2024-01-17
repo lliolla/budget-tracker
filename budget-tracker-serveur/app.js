ac5b0d37e85012c799612cabb90b03167f061fc7
lliolla <lliolla@hotmail.com>
2024-01-17 07:20:46 +0100

    FEAT:serveur fonctionnel

--- budget-tracker-serveur/app.js
+++ budget-tracker-serveur/app.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const app = express();
 
- //Connexion à la base de données MongoDB
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://limule38:TestBudget38@cluster0.4rk54y1.mongodb.net/',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+ 
 
   // Eviter les erros CORS afin que tout le monde puisse faire des requêtes
 app.use((req, res, next) => {
