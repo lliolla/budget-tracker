2476d33d0dd1e7ea82c085390b099dada607ae75
lliolla <lliolla@hotmail.com>
2024-01-23 16:37:08 +0100

    FEAT: CRUD categories fonctionnel

--- budget-tracker-serveur/controllers/category.js
+++ budget-tracker-serveur/controllers/category.js
@@ -1,5 +1,5 @@
 const Category = require('../models/category');
-
+const mongoose = require('mongoose');
 
 
 exports.getAllCategory = async (req, res) => {
@@ -20,21 +20,36 @@ exports.getOneCategory = async (req, res) => {
     }
   }; 
 
-exports.createCategory = async (req, res) => {
-  const { userId, budget, title } = req.body;
+  exports.createCategory = async (req, res) => {
+    const { budget, title } = req.body;
+  
+    try {
+      const id = new mongoose.Types.ObjectId(req.params.id);
 
-  try {
-    const newCategory = new Category({
-        userId, 
+      const newCategory = new Category({
+        userId: id,
         budget,
         title
-    });
-
-    const savedCategory = await newCategory.save();
-    res.status(201).json(savedCategory);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+      });
+  
+      const savedCategory = await newCategory.save();
+      res.status(201).json(savedCategory);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  exports.updateCategory = async (req, res) => {
+    let id=req.params.id
+    try {
+      const category = await Category.findByIdAndUpdate(
+        id,
+        req.body,
+        { new: true }
+      );
+      res.json(category);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
 };
 exports.deleteCategory = async (req, res) => {
     try {
@@ -44,15 +59,4 @@ exports.deleteCategory = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   }; 
-  exports.updateCategory = async (req, res) => {
-    try {
-        const category = await Category.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true }
-            );
-        res.json(category);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  }; 
+  
--- budget-tracker-serveur/models/category.js
+++ budget-tracker-serveur/models/category.js
@@ -5,7 +5,8 @@ const categorySchema = mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: true,
+    unique: true 
   },
   budget:{ type: Number, required: true, default:0 },
   title:{type: String,required: true }
